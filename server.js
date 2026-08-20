08f538c80377333c4a32ccfcee7283907e2d45db
bernardogarciagoncalves-png <bernardo.garciagoncalves@gmail.com>
2026-08-20 14:22:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.post('/api/executar-tarefa', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando com sucesso na porta ${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
